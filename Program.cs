ac6192893a037c91ca6fa5b0af4ac4b856b688b7
Marcin Hoppe <marcin.hoppe@particular.net>
2017-09-22 10:17:34 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var value in Enum.GetValues(typeof(Environment.SpecialFolder)))
+        {
+            var name = Enum.GetName(typeof(Environment.SpecialFolder), value);
+            var specialFolder = (Environment.SpecialFolder) value;
+            var folder = Environment.GetFolderPath(specialFolder, Environment.SpecialFolderOption.DoNotVerify);
+            Console.WriteLine($"{name}: {folder}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# DockerEnvVars
+# Docker Special Folders
+
+This is a sample .NET Core application that prints all the `System.Environment.SpecialFolder` values from within a Docker container.
+
+## How to use it?
+
+You need to build a container and run it using standard Docker command line tools:
+
+```
+C:\work\DockerSpecialFolders> docker build -t dockerspecialfolders .
+```
+
+Now the container is ready to run:
+
+```
+C:\work\DockerSpecialFolders> docker run dockerspecialfolders
+```
